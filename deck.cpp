8888ee455401222838f50927e34061ca4455869e
ArborChorus <tymchukoleksandr05@gmail.com>
2025-12-23 11:07:40 +0200

    Hand class

--- deck.cpp
+++ deck.cpp
@@ -1,3 +1,19 @@
 #include "deck.h"
 
-Deck::Deck() {}
+#include <random>
+
+Deck::Deck() {
+    for (int suit = 0; suit < 4; suit++){
+        for (int rank = 2; rank <= 14; rank++){
+            deckCards.append(Card(static_cast<Suit>(suit), static_cast<Rank>(rank)));
+        }
+    }
+}
+
+void Deck::Shuffle(){
+    std::shuffle(deckCards.begin(), deckCards.end(), std::mt19937{std::random_device{}()});
+}
+
+Card Deck::Draw(){
+    return deckCards.takeLast();
+}
